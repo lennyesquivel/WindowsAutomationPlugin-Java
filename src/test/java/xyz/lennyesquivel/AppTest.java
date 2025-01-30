f7fa3f7a52b5e414a178f588d986e0f7689eadd0
lennyesquivel <lenny.esquivel.fs@gmail.com>
2025-01-30 15:05:31 -0600

    Update pom and add tests

--- src/test/java/xyz/lennyesquivel/AppTest.java
+++ src/test/java/xyz/lennyesquivel/AppTest.java
@@ -1,38 +1,20 @@
-package org.example;
+package xyz.lennyesquivel;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    public void testApp() {
+        WinAPDriver driver = null;
+        try {
+            driver = new WinAPDriver("C:\\WinAPDriver\\publish\\WindowsAutomationPlugin.exe");
+            driver.launchStoreApp("Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            driver.quit();
+        } catch (Exception e) {
+            if (driver != null)
+                driver.quit();
+            throw new RuntimeException(e);
+        }
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
